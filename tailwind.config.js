b27d19f353df3121d5f468858c889a8af8c0399b
moises <moises@veiligwerk.net>
2024-03-19 17:11:18 +0100

    created astro project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  purge: ['./src/**/*.{astro,js,jsx,ts,tsx,vue}'],
+  // other configurations
+}
+
